f92d56096bc7754f430606a63c1b72e59e222277
SeanTseng2000 <f05350013@go.thu.edu.tw>
2023-03-02 23:31:12 +0800

    Chapter 5 practice

---
+++ site/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "jest":true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+
+    }
+}
--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const {engine:expressHandlebars}=require('express-handlebars')
+const handlers=require('./lib/handlers')
 
 const app=express()
 
@@ -16,29 +17,21 @@ const port=process.env.PORT||3000
 
 const fortune=require('./lib/fortune')
 
-app.get('/',(req,res)=>{
-res.render('home')
-})
+app.get('/',handlers.home)
 
-app.get('/about',(req,res)=>{
-
-  res.render('about',{fortune:fortune.getFortune})
-})
-
+app.get('/about',handlers.about)
 
 //自訂404頁面
-app.use((req,res)=>{
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 //自訂500頁面
-app.use((err,req,res,next)=>{
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
+if(require.main===module){
 app.listen(port,()=>{
     `Express started on http://localhost:${port};`+`press Ctrl-C to terminate.`
 })
+}
+else{
+    module.exports=app
+}
