186baa6cbcde442eece330215c3b94b92377695f
SeanTseng2000 <f05350013@go.thu.edu.tw>
2023-03-05 23:03:46 +0800

    CH7 practice

--- site/lib/handlers.js
+++ site/lib/handlers.js
@@ -9,3 +9,5 @@ exports.notFound=(req,res)=>res.render('404')
 /* eslint-disable no-unused-vars */
 exports.serverError=(err,req,res,next)=>res.render('500')
 /* eslint-enable no-unused-vars */
+
+exports.sectiontest=(req,res)=>res.render('sectiontest')
--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,12 +1,20 @@
 const express=require('express')
-const {engine:expressHandlebars}=require('express-handlebars')
+// const {engine:expressHandlebars}=require('express-handlebars')
+const expressHandlebars=require('express-handlebars')
 const handlers=require('./lib/handlers')
-
+const weatherMiddleware=require('./lib/middleware/weather')
 const app=express()
 
 //設置Handlebars view engine
-app.engine('handlebars',expressHandlebars({
+app.engine('handlebars',expressHandlebars.engine({
     defaultLayout:'main',
+    helpers:{
+        section:function(name,options){
+            if(!this._sections)this._sections={}
+            this._sections[name]=options.fn(this)
+            return null
+        }
+    }
 }))
 
 app.set('view engine','handlebars')
@@ -21,11 +29,15 @@ app.get('/',handlers.home)
 
 app.get('/about',handlers.about)
 
+app.get('/section-test',handlers.sectiontest)
+
 //自訂404頁面
 app.use(handlers.notFound)
 
 //自訂500頁面
 app.use(handlers.serverError)
+
+app.use(weatherMiddleware)
 
 if(require.main===module){
 app.listen(port,()=>{
