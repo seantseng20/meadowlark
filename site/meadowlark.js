d4481175fe8311897a252eb17617d42bcb0d89de
SeanTseng2000 <f05350013@go.thu.edu.tw>
2023-03-13 00:33:45 +0800

    CH9 practice

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -2,10 +2,13 @@ const express=require('express')
 // const {engine:expressHandlebars}=require('express-handlebars')
 const expressHandlebars=require('express-handlebars')
 const bodyParser=require('body-parser')
+const expressSession=require('express-session')
+const cookieParser=require('cookie-parser')
+const {credentials}=require('./config')
 const multiparty=require('multiparty')
 const handlers=require('./lib/handlers')
 const weatherMiddleware=require('./lib/middleware/weather')
-
+const flashMiddleware=require('./lib/middleware/flash')
 
 const app=express()
 
@@ -26,11 +29,22 @@ app.set('view engine','handlebars')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
+app.use(cookieParser(credentials.cookieSecret))
+
+app.use(expressSession({
+    resave:false,
+    saveUninitialized:false,
+    secret:credentials.cookieSecret,
+}))
+
 app.use(express.static(__dirname+'/public'))
 
 const port=process.env.PORT||3000
 
 const fortune=require('./lib/fortune')
+
+app.use(weatherMiddleware)
+app.use(flashMiddleware)
 
 app.get('/',handlers.home)
 
@@ -43,7 +57,6 @@ app.post('api/newsletter-signup',handlers.api.newsletterSignup)
 app.get('/newsletter-signup',handlers.newsletterSignup)
 app.post('/newsletter-signup/process',handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you',handlers.newsletterSignupThankyou)
-
 app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
 app.post('/contest/vacation-photo/:year/:month',(req,res)=>{
     const form=new multiparty.Form()
@@ -54,6 +67,9 @@ app.post('/contest/vacation-photo/:year/:month',(req,res)=>{
 })
 
 app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou)
+
+app.get('/newsletter-archive', handlers.newsletterArchive)
+
 
 //自訂404頁面
 app.use(handlers.notFound)
