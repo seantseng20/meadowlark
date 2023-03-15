7855ba15e02ac086a7b950f98b61eb040cad4f0a
SeanTseng2000 <f05350013@go.thu.edu.tw>
2023-03-15 23:15:18 +0800

    CH9 session practice

--- site/lib/handlers.js
+++ site/lib/handlers.js
@@ -113,5 +113,4 @@ const VALID_EMAIL_REGEX = new RegExp('^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@' +
   '[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?' +
   '(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$')
 
-
-  exports.newsletterArchive = (req, res) => res.render('newsletter-archive')
+exports.newsletterArchive = (req, res) => res.render('newsletter-archive')
